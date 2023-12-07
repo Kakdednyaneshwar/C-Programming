48e9548f221f650bd9c8f0fce8b540eb9e3f2b4d
Kakde Dnyaneshwar <118787516+Kakdednyaneshwar@users.noreply.github.com>
2023-12-07 07:41:18 -0800

    Arithmetic Operation

---
+++ Addition.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Enter the two numbwer:"<<endl;
+
+    int n1=0,n2=0;
+
+    cin>>n1>>n2;
+
+    cout<<"Sum of number:"<<n1<<"and"<<n2<<"is:"<<n1+n2<<endl;
+
+    cout<<"Sub of number:"<<n1<<"and"<<n2<<"is:"<<n1-n2<<endl;
+
+    cout<<"Multi of number:"<<n1<<"and"<<n2<<"is:"<<n1*n2<<endl;
+
+    cout<<"Div of number:"<<n1<<"and"<<n2<<"is:"<<n1/n2<<endl;
+
+    cout<<"Modu of number:"<<n1<<"and"<<n2<<"is:"<<n1+n2<<endl;
+
+    return 0;
+
+}
