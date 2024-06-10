0d5006eab739182989aab4a95c68c85c12585c60
Kakde Dnyaneshwar <118787516+Kakdednyaneshwar@users.noreply.github.com>
2024-06-10 22:10:19 +0530

    If Statement

---
+++ if.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int num=10;
+
+    if(num>20)
+    {
+        cout<<"number is greter than 20";
+
+    }
+    cout<<"number is not greater then 20";
+
+    return 0;
+}
