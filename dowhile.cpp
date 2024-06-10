051da824095e71058f568859b04f62598e607e37
Kakde Dnyaneshwar <118787516+Kakdednyaneshwar@users.noreply.github.com>
2024-06-10 22:16:24 +0530

    Do While

---
+++ dowhile.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    char a='A';
+
+    do
+    {
+        cout<<a<<" ";
+        a++;
+    }
+     while (a<='Z');
+
+    return 0;
+    
+    
+}
+
