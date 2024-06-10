7c0802c3ba8e9c4345a485a93817f3609e084f6b
Kakde Dnyaneshwar <118787516+Kakdednyaneshwar@users.noreply.github.com>
2024-06-10 22:14:57 +0530

    If Else Statement

---
+++ ifelse.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int num;
+
+    cout<<"Enter the value = ";
+    cin>>num;
+
+    if(num%2==0)
+    {
+        cout<<num<<" is even number";
+    }
+    else
+    {
+        cout<<num<<" is odd number";
+    }
+    return 0;
+}
