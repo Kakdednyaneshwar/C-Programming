e800ec257e738dff38baf25b16ea8198eed77fa8
Kakde Dnyaneshwar <118787516+Kakdednyaneshwar@users.noreply.github.com>
2024-06-10 22:16:59 +0530

    Simple For Loop

---
+++ for.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int count=0;
+
+    for(count=1;count<=10;count++)
+    {
+        cout<<"Hello There"<<endl;
+    }
+    return 0;
+
+}
